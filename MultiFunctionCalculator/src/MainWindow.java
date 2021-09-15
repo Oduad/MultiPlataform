899202114e494c756cf803b636a66debe454166e
Oduad <aragon_1123@hotmail.com>
2021-09-14 22:04:43 -0500

    jMenuItem1 modificated and Exit button implemented

--- MultiFunctionCalculator/src/MainWindow.java
+++ MultiFunctionCalculator/src/MainWindow.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -11,7 +12,7 @@ public class MainWindow extends JFrame {
 
 	JPanel panel;
 	JMenuBar menuBar;
-	JMenu operations, help, plots;
+	JMenu operations, help, plots, games;
 	JMenuItem jMenuItem1, jMenuItem2, jMenuItem3, jMenuItem4, jMenuItem5, jMenuItem6;
 
 	public MainWindow() {
@@ -32,7 +33,7 @@ public class MainWindow extends JFrame {
 	private void setPanel() {
 		panel = new JPanel();
 		// setSize(300,450);
-		//panel.setBackground(Color.magenta);
+		// panel.setBackground(Color.magenta);
 		panel.setLayout(null);
 		this.add(panel);
 	}
@@ -42,27 +43,58 @@ public class MainWindow extends JFrame {
 		operations = new JMenu("Operations");
 		plots = new JMenu("Plots");
 		help = new JMenu("Help");
+		games = new JMenu("Games");
 		menuBar.setBounds(0, 0, 400, 25);
 		// menuBar.setBackground(Color.yellow);
 		jMenuItem1 = new JMenuItem("Aritmetic operations");
-		jMenuItem1.addActionListener(null);
+		aritmeticActionPerformed();
 		jMenuItem2 = new JMenuItem("Algebra operations");
+		algebraActionPerformed();
 		jMenuItem3 = new JMenuItem("Vector operations");
 		jMenuItem4 = new JMenuItem("Algebra operations");
 		jMenuItem5 = new JMenuItem("Numerical Methods");
 		jMenuItem6 = new JMenuItem("Exit");
+		close();
 		menuBar.add(operations);
 		menuBar.add(plots);
 		menuBar.add(help);
+		menuBar.add(games);
 		operations.add(jMenuItem1);
 		operations.add(jMenuItem2);
 		operations.add(jMenuItem3);
 		operations.add(jMenuItem4);
 		operations.add(jMenuItem5);
+		operations.add(jMenuItem6);
 		setJMenuBar(menuBar);
 	}
-	
-	private void aritmeticActionPerformed(ActionEvent ae) {
-		WindowArithmetic wa = new WindowArithmetic();
+
+	private void aritmeticActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				WindowArithmetic wa = new WindowArithmetic();
+			}
+		};
+		jMenuItem1.addActionListener(action);
+	}
+
+	private void algebraActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				WindowAlgebra wAlgebra = new WindowAlgebra();
+			}
+		};
+		jMenuItem2.addActionListener(action);
+	}
+
+	private void close() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		};
+		jMenuItem6.addActionListener(action);
 	}
 }
