c34ab3f7ee883cafb503b286c3d2856a04d3882b
Oduad <aragon_1123@hotmail.com>
2021-09-24 15:29:16 -0500

    Buttons 0-9, addition, division, muliplication, division, radix, dot, erase, conversion, clear and equals configurated.

--- MultiFunctionCalculator/src/WindowArithmetic.java
+++ MultiFunctionCalculator/src/WindowArithmetic.java
@@ -16,7 +16,7 @@ public class WindowArithmetic extends JFrame {
 	GridBagLayout gbl;
 	JLabel showNumbers, show;
 	JButton jb0, jb1, jb2, jb3, jb4, jb5, jb6, jb7, jb8, jb9;
-	JButton jbc, jbPower, jbDivision, jbEraise, jbMultiplication, jbDifference, jbAddition;
+	JButton jbClear, jbRadix, jbDivision, jbEraise, jbMultiplication, jbDifference, jbAddition;
 	JButton jbConvert, jbDot, jbEquals;
 	private String numbersString = "";
 	private String operation = "null";
@@ -65,35 +65,39 @@ public class WindowArithmetic extends JFrame {
 		gbc.gridheight = 2;
 		panel.add(showNumbers, gbc);
 
-		jbc = new JButton("  C  ");
+		jbClear = new JButton("C");
 		gbc.gridx = 0;
 		gbc.gridy = 3;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
-		panel.add(jbc, gbc);
+		panel.add(jbClear, gbc);
+		clearActionPerformed();
 
-		jbPower = new JButton("Pow");
+		jbRadix = new JButton("√");
 		gbc.gridx = 1;
 		gbc.gridy = 3;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
-		panel.add(jbPower, gbc);
+		panel.add(jbRadix, gbc);
+		radixActionPerformed();
 
-		jbDivision = new JButton("  / ");
+		jbDivision = new JButton("/");
 		gbc.gridx = 2;
 		gbc.gridy = 3;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		panel.add(jbDivision, gbc);
+		divisionActionPerformed();
 
-		jbEraise = new JButton("<--");
+		jbEraise = new JButton("←");
 		gbc.gridx = 3;
 		gbc.gridy = 3;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		panel.add(jbEraise, gbc);
+		eraseActionPerformed();
 
-		jb7 = new JButton("  7  ");
+		jb7 = new JButton("7");
 		gbc.gridx = 0;
 		gbc.gridy = 4;
 		gbc.gridwidth = 1;
@@ -101,7 +105,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb7, gbc);
 		button7ActionPerformed();
 
-		jb8 = new JButton("  8  ");
+		jb8 = new JButton("8");
 		gbc.gridx = 1;
 		gbc.gridy = 4;
 		gbc.gridwidth = 1;
@@ -109,7 +113,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb8, gbc);
 		button8ActionPerformed();
 
-		jb9 = new JButton("  9  ");
+		jb9 = new JButton("9");
 		gbc.gridx = 2;
 		gbc.gridy = 4;
 		gbc.gridwidth = 1;
@@ -117,14 +121,15 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb9, gbc);
 		button9ActionPerformed();
 
-		jbMultiplication = new JButton("  X  ");
+		jbMultiplication = new JButton("X");
 		gbc.gridx = 3;
 		gbc.gridy = 4;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		panel.add(jbMultiplication, gbc);
+		multiplicationActionPerformed();
 
-		jb4 = new JButton("  4  ");
+		jb4 = new JButton("4");
 		gbc.gridx = 0;
 		gbc.gridy = 5;
 		gbc.gridwidth = 1;
@@ -132,7 +137,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb4, gbc);
 		button4ActionPerformed();
 
-		jb5 = new JButton("  5  ");
+		jb5 = new JButton("5");
 		gbc.gridx = 1;
 		gbc.gridy = 5;
 		gbc.gridwidth = 1;
@@ -140,7 +145,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb5, gbc);
 		button5ActionPerformed();
 
-		jb6 = new JButton("  6  ");
+		jb6 = new JButton("6");
 		gbc.gridx = 2;
 		gbc.gridy = 5;
 		gbc.gridwidth = 1;
@@ -148,7 +153,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb6, gbc);
 		button6ActionPerformed();
 
-		jbDifference = new JButton("  -  ");
+		jbDifference = new JButton("—");
 		gbc.gridx = 3;
 		gbc.gridy = 5;
 		gbc.gridwidth = 1;
@@ -156,7 +161,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jbDifference, gbc);
 		differenceActionPerformed();
 
-		jb1 = new JButton("  1  ");
+		jb1 = new JButton("1");
 		gbc.gridx = 0;
 		gbc.gridy = 6;
 		gbc.gridwidth = 1;
@@ -164,7 +169,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb1, gbc);
 		button1ActionPerformed();
 
-		jb2 = new JButton("  2  ");
+		jb2 = new JButton("2");
 		gbc.gridx = 1;
 		gbc.gridy = 6;
 		gbc.gridwidth = 1;
@@ -172,7 +177,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb2, gbc);
 		button2ActionPerformed();
 
-		jb3 = new JButton("  3  ");
+		jb3 = new JButton("3");
 		gbc.gridx = 2;
 		gbc.gridy = 6;
 		gbc.gridwidth = 1;
@@ -180,7 +185,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb3, gbc);
 		button3ActionPerformed();
 
-		jbAddition = new JButton("  +  ");
+		jbAddition = new JButton("+");
 		gbc.gridx = 3;
 		gbc.gridy = 6;
 		gbc.gridwidth = 1;
@@ -188,14 +193,15 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jbAddition, gbc);
 		additionActionPerformed();
 
-		jbConvert = new JButton(" +- ");
+		jbConvert = new JButton("+-");
 		gbc.gridx = 0;
 		gbc.gridy = 7;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		panel.add(jbConvert, gbc);
+		conversionActionPerformed();
 
-		jb0 = new JButton("  0  ");
+		jb0 = new JButton("0");
 		gbc.gridx = 1;
 		gbc.gridy = 7;
 		gbc.gridwidth = 1;
@@ -203,7 +209,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jb0, gbc);
 		button0ActionPerformed();
 
-		jbDot = new JButton("  . ");
+		jbDot = new JButton(".");
 		gbc.gridx = 2;
 		gbc.gridy = 7;
 		gbc.gridwidth = 1;
@@ -211,7 +217,7 @@ public class WindowArithmetic extends JFrame {
 		panel.add(jbDot, gbc);
 		dotActionPerformed();
 
-		jbEquals = new JButton(" = ");
+		jbEquals = new JButton("=");
 		gbc.gridx = 3;
 		gbc.gridy = 7;
 		gbc.gridwidth = 1;
@@ -225,7 +231,11 @@ public class WindowArithmetic extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (numbersString != "") {
-					numbersString += "0";
+					if (showNumbers.getText() == "0") {
+						numbersString += "0";
+					} else {
+						numbersString += "0";
+					}
 					showNumbers.setText(numbersString);
 					enabled = true;
 				}
@@ -238,7 +248,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "1";
+				if (showNumbers.getText() == "0") {
+					numbersString += "1";
+				} else {
+					numbersString += "1";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -250,7 +264,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "2";
+				if (showNumbers.getText() == "0") {
+					numbersString += "2";
+				} else {
+					numbersString += "2";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -262,7 +280,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "3";
+				if (showNumbers.getText() == "0") {
+					numbersString += "3";
+				} else {
+					numbersString += "3";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -274,7 +296,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "4";
+				if (showNumbers.getText() == "0") {
+					numbersString += "4";
+				} else {
+					numbersString += "4";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -286,7 +312,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "5";
+				if (showNumbers.getText() == "0") {
+					numbersString += "5";
+				} else {
+					numbersString += "5";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -298,7 +328,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "6";
+				if (showNumbers.getText() == "0") {
+					numbersString += "6";
+				} else {
+					numbersString += "6";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -310,7 +344,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "7";
+				if (showNumbers.getText() == "0") {
+					numbersString += "7";
+				} else {
+					numbersString += "7";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -322,7 +360,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "8";
+				if (showNumbers.getText() == "0") {
+					numbersString += "8";
+				} else {
+					numbersString += "8";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -334,7 +376,11 @@ public class WindowArithmetic extends JFrame {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				numbersString += "9";
+				if (showNumbers.getText() == "0") {
+					numbersString += "9";
+				} else {
+					numbersString += "9";
+				}
 				showNumbers.setText(numbersString);
 				enabled = true;
 			}
@@ -347,16 +393,48 @@ public class WindowArithmetic extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (enabled = true) {
-					number1 = Double.parseDouble(numbersString);
-					show.setText(numbersString + " + ");
-					numbersString = "";
+					number1 = Double.parseDouble(numbersString); // cadena números
+					show.setText(numbersString + " + ");// Etiqueta muestra y cadena números
+					numbersString = ""; // Cadena números
 					operation = "add";
 					enabled = false;
-					dot = true;
 				}
 			}
 		};
 		jbAddition.addActionListener(action);
+	}
+
+	private void conversionActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (numbersString.charAt(0) != '-') {
+					numbersString = "-" + numbersString;
+				} else {
+					numbersString = numbersString.substring(1, numbersString.length());
+				}
+				showNumbers.setText(numbersString);
+			}
+		};
+		jbConvert.addActionListener(action);
+	}
+
+	private void eraseActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int size = numbersString.length();
+				if (size > 0) {
+					if (size == 1) {
+						numbersString = "0";
+					} else {
+						numbersString = numbersString.substring(0, numbersString.length() - 1);
+					}
+					showNumbers.setText(numbersString);
+				}
+			}
+		};
+		jbEraise.addActionListener(action);
 	}
 
 	private void differenceActionPerformed() {
@@ -365,33 +443,84 @@ public class WindowArithmetic extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if (enabled = true) {
 					number1 = Double.parseDouble(numbersString);
-					show.setText(numbersString + " + ");
+					show.setText(numbersString + "-");
 					numbersString = "";
-					operation = "add";
+					operation = "dif";
 					enabled = false;
 					dot = true;
 				}
 			}
 		};
-		jbAddition.addActionListener(action);
+		jbDifference.addActionListener(action);
 	}
-	
+
+	private void multiplicationActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (enabled = true) {
+					number1 = Double.parseDouble(numbersString);
+					show.setText(numbersString + "x");
+					numbersString = "";
+					operation = "mul";
+					enabled = false;
+					dot = true;
+				}
+			}
+		};
+		jbMultiplication.addActionListener(action);
+	}
+
+	private void divisionActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (enabled = true) {
+					number1 = Double.parseDouble(numbersString);
+					show.setText(numbersString + "x");
+					numbersString = "";
+					operation = "div";
+					enabled = false;
+					dot = true;
+				}
+			}
+		};
+		jbDivision.addActionListener(action);
+	}
+
+	private void radixActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				number1 = Double.parseDouble(numbersString);
+				show.setText("√" + numbersString + "");
+				numbersString = "";
+				result = Math.sqrt(number1);
+				showNumbers.setText(String.format("%.2f", result)); // etiqueta numeros
+				numbersString = String.valueOf(result); // Cadena números
+				operation = "null";
+				dot = true;
+			}
+		};
+		jbRadix.addActionListener(action);
+	}
+
 	private void dotActionPerformed() {
 		ActionListener action = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (dot == true) {
-					if (numbersString == ".") {
+					if (numbersString == "") {
 						numbersString += "0.";
 					} else {
 						numbersString += ".";
 					}
-					showNumbers.setText(numbersString);
+					showNumbers.setText(numbersString); // Etiqueta numeros y cadena numeros
 					dot = false;
 				}
 			}
 		};
-		jbAddition.addActionListener(action);
+		jbDot.addActionListener(action);
 	}
 
 	private void equalsActionPerformed() {
@@ -401,19 +530,52 @@ public class WindowArithmetic extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (operation.equals("null")) {
-					showNumbers.setText(numbersString);
+					showNumbers.setText(numbersString); // etiqueta números y cadena números
 				} else if (operation.equals("add")) {
-					number2 = Double.parseDouble(numbersString);
+					number2 = Double.parseDouble(numbersString); // cadena números
 					result = number1 + number2;
-					showNumbers.setText(String.format("%.2f", result));
-					numbersString = String.valueOf(result);
+					showNumbers.setText(String.format("%.2f", result)); // etiqueta numeros
+					numbersString = String.valueOf(result); // Cadena números
+					operation = "null";
+				} else if (operation.equals("dif")) {
+					number2 = Double.parseDouble(numbersString); // cadena números
+					result = number1 - number2;
+					showNumbers.setText(String.format("%.2f", result)); // etiqueta numeros
+					numbersString = String.valueOf(result); // Cadena números
+					operation = "null";
+				} else if (operation.equals("mul")) {
+					number2 = Double.parseDouble(numbersString); // cadena números
+					result = number1 * number2;
+					showNumbers.setText(String.format("%.2f", result)); // etiqueta numeros
+					numbersString = String.valueOf(result); // Cadena números
+					operation = "null";
+				} else if (operation.equals("div")) {
+					number2 = Double.parseDouble(numbersString); // cadena números
+					result = number1 / number2;
+					showNumbers.setText(String.format("%.2f", result)); // etiqueta numeros
+					numbersString = String.valueOf(result); // Cadena números
 					operation = "null";
 				}
-				showNumbers.setText("");
+				show.setText("");
 				enabled = true;
 			}
 		};
 		jbEquals.addActionListener(action);
 		enabled = true;
+	}
+
+	private void clearActionPerformed() {
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				showNumbers.setText(numbersString);
+				showNumbers.setText("0");
+				numbersString = "";
+				operation = "null";
+				enabled = true;
+				dot = true;
+			}
+		};
+		jbClear.addActionListener(action);
 	}
 }
