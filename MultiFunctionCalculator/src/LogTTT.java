96a744173e2e4530797282bafc77ffc1bc711a35
Oduad <aragon_1123@hotmail.com>
2021-10-13 12:11:55 -0500

    Created a JFrame for LogTTT.

---
+++ MultiFunctionCalculator/src/LogTTT.java
@@ -0,0 +1,24 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LogTTT extends JFrame{
+
+	private JPanel panel;
+	private JButton buttonGuardar, buttonEnter;
+	private JLabel name1, name2;
+	private JTextField textName1, textName2;
+	
+	public LogTTT() {
+		setVisible(true);
+		setLocationRelativeTo(null);
+		
+	}
+	
+	private void initComponents() {
+		
+	}
+	
+}
