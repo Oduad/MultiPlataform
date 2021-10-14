778f03e0d3904543f9a73c10ee6b8a186486714f
Oduad <aragon_1123@hotmail.com>
2021-10-14 16:18:34 -0500

    Recall of the instance of FirstWindow.

--- MultiFunctionCalculator/src/main/Main.java
+++ MultiFunctionCalculator/src/main/Main.java
@@ -5,7 +5,7 @@ import classes.FirstWindow;
 public class Main {
 
 	public static void main(String[] args) {
-		FirstWindow login = new FirstWindow();
+		FirstWindow firstWindow = new FirstWindow();
 	}
 }
   
